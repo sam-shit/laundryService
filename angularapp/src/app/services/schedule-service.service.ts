4188ac6a472bd5441802371a5873ae8a4450141a
Iamneo <techadmin@iamneo.ai>
2023-11-24 10:38:15 +0000

    Backup Commit

--- angularapp/src/app/services/schedule-service.service.ts
+++ angularapp/src/app/services/schedule-service.service.ts
@@ -1,19 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ScheduleServiceService {
+export class ScheduleService {
 
-  private url = 'https://8080-eccdeabbadafaab307795213daadeafcadfone.premiumproject.examly.io/api/laundry'
-
-  constructor(private httpclient : HttpClient ) { }
-
-  getPackages() : Observable(<any[]>){
-
-    return this.httpclient.get<any[]>(this.url + '/')
-
+  private apiBaseUrl = 'https://8080-eccdeabbadafaab307795213daadeafcadfone.premiumproject.examly.io/api';
+  constructor(private httpclient : HttpClient) { }
+  
+  getPackages() : Observable<any[]>{
+    return this.httpclient.get<any[]>(this.apiBaseUrl + '/laundry/packages');
+  }
+  getStatus() : Observable<any[]>{
+    return this.httpclient.get<any[]>(this.apiBaseUrl + '/laundry/status');
+  }
+  getSchedule() : Observable<any[]>{
+    return this.httpclient.get<any[]>(this.apiBaseUrl + '/laundry/schedules');
+  }
+  httpOptions = {headers : new HttpHeaders({'content-type' : 'application/json'})}
+  
+  createSchedule(scheduleData : any) : Observable<any[]>{
+    return this.httpclient.post<any[]>(this.apiBaseUrl + '/laundry/schedule/add' ,scheduleData,this.httpOptions);
+  }
+  updateSchedule(scheduleId : number ,statusId : number) : Observable<any[]>{
+    return this.httpclient.put<any[]>(this.apiBaseUrl + '/laundry/schedule/update' + scheduleId + statusId , this.httpOptions);
   }
 }
