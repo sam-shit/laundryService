132bf7431bbe790429affa429b1d35a3f603d8e6
Iamneo <techadmin@iamneo.ai>
2023-11-24 09:53:18 +0000

    Backup Commit

---
+++ angularapp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { ScheduleFormComponent } from './schedule-form/schedule-form.component';
+import { ScheduleDetailsComponent } from './schedule-details/schedule-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ScheduleComponent,
+    ScheduleFormComponent,
+    ScheduleDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
