cbf1c09407883c59ba4e24f312f8d396581cf6e0
Iamneo <techadmin@iamneo.ai>
2023-11-24 11:49:36 +0000

    first

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ScheduleFormComponent } from './schedule-form/schedule-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '/laundry/schedule/add', component : ScheduleFormComponent}
+  // {path : '/laundry/schedule/', component : ScheduleFormComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
